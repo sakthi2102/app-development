dadbf9df56846dd6a1b83ef2fd57e75d6a35093b
SAKTHI VEL <veerasakthivel130@gmail.com>
2023-03-20 22:06:09 +0530

    commit

--- app/src/main/java/com/example/trackmyroute/MapsActivity.java
+++ app/src/main/java/com/example/trackmyroute/MapsActivity.java
@@ -1,7 +1,6 @@
 package com.example.trackmyroute;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -11,24 +10,20 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.widget.FrameLayout;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
+
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
-import android.Manifest;
+
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -37,9 +32,6 @@ import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     Location currentLocation;
-    FusedLocationProviderClient fusedClient;
-    private static final int REQUEST_CODE = 101;
-    FrameLayout map;
     GoogleMap gmap;
     Marker marker;
     SearchView searchView;
@@ -53,13 +45,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        map = findViewById(R.id.map);
+
         searchView = findViewById(R.id.search);
         searchView.clearFocus();
 
-        fusedClient = LocationServices.getFusedLocationProviderClient(this);
-        getLocation();
-
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapview);
+        supportMapFragment.getMapAsync(this);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -95,46 +86,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            return;
-        }
-        Task<Location> task = fusedClient.getLastLocation();
-
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    currentLocation = location;
-                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-                    assert supportMapFragment != null;
-                    supportMapFragment.getMapAsync(MapsActivity.this);
-                }
-            }
-        });
-    }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.gmap = googleMap;
-        LatLng latlng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("My Current Location");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 13));
-        googleMap.addMarker(markerOptions);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_CODE){
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLocation();
-            }
+        gmap = googleMap;
+        LatLng latLng = new LatLng(0, 0);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("Current Location");
+        markerOptions.position(latLng);
+        gmap.addMarker(markerOptions);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+        gmap.animateCamera(cameraUpdate);
+        gmap.getUiSettings().setAllGesturesEnabled(true);
+        gmap.getUiSettings().setCompassEnabled(true);
+        gmap.getUiSettings().setZoomControlsEnabled(true);
+        gmap.getUiSettings().setZoomGesturesEnabled(true);
+        gmap.getUiSettings().setScrollGesturesEnabled(true);
+        gmap.getUiSettings().setRotateGesturesEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        gmap.setMyLocationEnabled(true);
     }
 }
