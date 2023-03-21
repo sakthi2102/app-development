a84f411ef715851034083379e8dd7432f15a2f28
SAKTHI VEL <veerasakthivel130@gmail.com>
2023-03-22 02:42:20 +0530

    commit

--- app/src/main/java/com/example/trackmyroute/MapsActivity.java
+++ app/src/main/java/com/example/trackmyroute/MapsActivity.java
@@ -1,16 +1,36 @@
 package com.example.trackmyroute;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
+
+import android.location.LocationManager;
+
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import com.google.android.gms.location.LocationRequest;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,38 +39,44 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    Location currentLocation;
+    LocationRequest locationRequest;
+
     GoogleMap gmap;
     Marker marker;
     SearchView searchView;
 
 
-    public boolean isMyLocationButtonEnabled() {
-        return true;
-    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
         searchView = findViewById(R.id.search);
         searchView.clearFocus();
-
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapview);
         supportMapFragment.getMapAsync(this);
+        CheckGps();
+
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -86,24 +112,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    private void CheckGps() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addAllLocationRequests(Collections.singleton(locationRequest))
+                .setAlwaysShow(true);
+        Task<LocationSettingsResponse> locationSettingsRequestTask = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+        locationSettingsRequestTask.addOnCompleteListener(task -> {
+            try {
+                LocationSettingsResponse response = task.getResult(ApiException.class);
+                Toast.makeText(MapsActivity.this, "Gps Already Enabled", Toast.LENGTH_SHORT).show();
+                GetLocationUpdate();
+            } catch (ApiException e) {
+                if (e.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                    ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                    try {
+                        resolvableApiException.startResolutionForResult(MapsActivity.this, 101);
+                    } catch (IntentSender.SendIntentException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                if (e.getStatusCode() == LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE) {
+                    Toast.makeText(MapsActivity.this, "Settings Not Availabe", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        gmap = googleMap;
-        LatLng latLng = new LatLng(0, 0);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.title("Current Location");
-        markerOptions.position(latLng);
-        gmap.addMarker(markerOptions);
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
-        gmap.animateCamera(cameraUpdate);
-        gmap.getUiSettings().setAllGesturesEnabled(true);
-        gmap.getUiSettings().setCompassEnabled(true);
-        gmap.getUiSettings().setZoomControlsEnabled(true);
-        gmap.getUiSettings().setZoomGesturesEnabled(true);
-        gmap.getUiSettings().setScrollGesturesEnabled(true);
-        gmap.getUiSettings().setRotateGesturesEnabled(true);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MapsActivity.this, "Gps Enabled", Toast.LENGTH_SHORT).show();
+            }
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(MapsActivity.this, "Denied Gps", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("ServiceCast")
+    private void GetLocationUpdate()
+    {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -113,6 +173,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        Location location = locationManager.getLastKnownLocation(provider);
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        LatLng latLng = new LatLng(lat, lng);
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Current Location");
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng )      // Sets the center of the map to Mountain View
+                .zoom(20)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        gmap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        gmap.addMarker(markerOptions);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
+        gmap=googleMap;
+        gmap.getUiSettings().setAllGesturesEnabled(true);
+        gmap.getUiSettings().setCompassEnabled(true);
+        gmap.getUiSettings().setZoomControlsEnabled(true);
+        gmap.getUiSettings().setZoomGesturesEnabled(true);
+        gmap.getUiSettings().setScrollGesturesEnabled(true);
+        gmap.getUiSettings().setRotateGesturesEnabled(true);
+        gmap.getUiSettings().setTiltGesturesEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
         gmap.setMyLocationEnabled(true);
+        gmap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                CheckGps();
+                return false;
+            }
+        });
     }
 }
