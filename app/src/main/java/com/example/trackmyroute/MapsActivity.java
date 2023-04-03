93a069a381f2bdf57f103864ffb2f19e7a9db3b3
SAKTHI VEL <veerasakthivel130@gmail.com>
2023-04-03 19:13:29 +0530

    commit

--- app/src/main/java/com/example/trackmyroute/MapsActivity.java
+++ app/src/main/java/com/example/trackmyroute/MapsActivity.java
@@ -27,8 +27,6 @@ import androidx.fragment.app.FragmentActivity;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
